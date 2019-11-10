5182cc9998164cc8bb94d46789e20e9766b20f4e
unknown <muhannadjudah@gmail.com>
2019-11-10 09:33:54 +0100

    Updates

---
+++ precache-manifest.8b332ecc5b50481532ea59527247ccb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de18bd183c5102a6c5a0fb517f441393",
+    "url": "/github.io/Recipes/index.html"
+  },
+  {
+    "revision": "55e63ab3bfb67fa38d59",
+    "url": "/github.io/Recipes/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "935b93dd950da68a6d4a",
+    "url": "/github.io/Recipes/static/css/main.87f03a96.chunk.css"
+  },
+  {
+    "revision": "55e63ab3bfb67fa38d59",
+    "url": "/github.io/Recipes/static/js/2.7ebf1ab4.chunk.js"
+  },
+  {
+    "revision": "935b93dd950da68a6d4a",
+    "url": "/github.io/Recipes/static/js/main.79662201.chunk.js"
+  },
+  {
+    "revision": "32e416cb10e9fd4b7469",
+    "url": "/github.io/Recipes/static/js/runtime-main.08f415d4.js"
+  }
+]);
